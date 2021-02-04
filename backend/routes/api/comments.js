7c6d0fd72b920f73adf090a9356700e3dab286cb
Angelica <angelica.wilson93@gmail.com>
2021-02-04 17:43:32 -0600

    Created backend route for creating comment. Functional. Needs to be styled

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -20,21 +20,15 @@ router.get('/:goalId',
   })
 );
 
+// CREATE A COMMENT
+router.post('/:goalId(\\d+)',
+  asyncHandler(async (req, res) => {
+    const {userId, goalId, comment} = req.body
+    const newComment = await Comment.create({ userId, goalId, comment });
+    await newComment.save();
+    return res.json({ newComment });
+  })
+);
 
-// router.post('/:goalId',
-//   asyncHandler(async (req, res) => {
-//     const goalId = req.params.goalId;
-//     const comments = await Comment.findAll({
-//       where: { goalId},
-//       include: [
-//         {
-//           model: User,
-//         },
-//       ],
-//     });
-
-//     return res.json(comments)
-//   })
-// );
 
 module.exports = router;
--- frontend/src/components/CommentForm/index.js
+++ frontend/src/components/CommentForm/index.js
@@ -1,34 +1,35 @@
 import { useState, useEffect } from "react";
 import {useDispatch, useSelector} from 'react-redux';
+import {createComment} from '../../store/comments';
 // import "./CommentForm.css";
 
 function CommentForm({visible, goalId}) {
   const [newComment, setNewComment] = useState('');
   const [errors, setErrors] = useState([]);
+  const [vis, setVisible] = useState(true)
+
+  const dispatch = useDispatch();
 
   const userId = useSelector(state => state.session.user.id);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (newComment) {
       setErrors([]);
-      let res = await fetch(`/api/comments/${goalId}`, {
-      method: "POST",
-      // headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        userId: userId,
-        goalId: goalId,
-        comment: newComment,
-      }),
-    });
-    res = await res.json();
-    console.log(userId, goalId, newComment, 'what up');
+    
+    return dispatch(createComment({userId, goalId, newComment}),
+      setNewComment(''),
+      // setVisible(false),
+    )
+      .catch(res => {
+          if (res.data && res.data.errors) setErrors(res.data.errors);
+        });
     } else {
       return setErrors(['Comment must not be empty']);
     }
-    
     }
 
-  if (!visible) return null
+  if (!visible || !vis) return null
     return (
     <>
       <form onSubmit={handleSubmit}>
