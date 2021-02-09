860011b63e3e650bb231d9cb3e809008afcd7f09
Angelica <angelica.wilson93@gmail.com>
2021-02-09 13:29:09 -0600

    Add style to profile button dropdown

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -34,14 +34,14 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
+        <i className="fas fa-user-circle profile-button" />
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
           <li>{user.username}</li>
           <li>{user.email}</li>
           <li>
-            <button onClick={logout}>Log Out</button>
+            <button className='nav-buttons'onClick={logout}>Log Out</button>
           </li>
         </ul>
       )}
