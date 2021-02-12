5e9ba278557b5e95f75ec2e04adaa81a7d72eac6
Angelica <angelica.wilson93@gmail.com>
2021-02-12 16:00:01 -0600

    Search page is displaying if the logged in user is following or unfolowing those searched users. A bit of styling

--- frontend/src/components/SearchPage/SearchedUserGoals.js
+++ frontend/src/components/SearchPage/SearchedUserGoals.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 
 function SearchedUserGoals({userId, username}) {
--- frontend/src/components/SearchPage/index.js
+++ frontend/src/components/SearchPage/index.js
@@ -1,22 +1,10 @@
-import {useParams} from "react-router-dom";
-import { useState } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {search} from '../../store/search.js';
-import {Link} from 'react-router-dom';
-import {useEffect} from 'react';
+import {useSelector} from 'react-redux';
 import SearchedUserGoalModal from './SearchedUserGoalModal';
+import SidePanel from "../SidePanel/index.js";
+import CheckIfFollowing from '../MyFollowersProfilePage/CheckIfFollowing';
+
 function SearchPage () {
-    const [showModal, setShowModal] = useState(false);
-//     const params = useParams();
-//     const {searchTerm} = params;
-    
-//     const dispatch = useDispatch()
-
-//     useEffect (() => {
-//         dispatch(search({searchTerm}))
-//     }, [])
-  
-
+ 
     const search = useSelector(state => {
         return state.search.search
     });
@@ -29,58 +17,56 @@ function SearchPage () {
 
         if (searchedUsers.length ===0){
             return (
-                <h1 className='h1'>No matching results</h1>
+                <>
+                    <div className='main-container'>
+                        <SidePanel />
+                        <div className='results-container'>
+                            <h1>No matching results</h1>
+                        </div>
+                    </div>
+                </>
             )
         } else {
             return (
-                searchedUsers.map(user => {
-                    const userId = user.id
-                    const username = user.username
-                return (
-                    <>
-                        <div key={user.id}>
-                            <button>
-                                <SearchedUserGoalModal userId={userId} username={username}/>
-                                {user.username}
-                                <SearchedUserGoalModal userId={userId} username={username}/>
-                            </button>
-                        </div>
-                    </>
-                )
-               
-            })
+                <div className='main-container'>
+                    <SidePanel />
+                    <div className='results-container'>
+                    {searchedUsers.map(user => {
+                        const userId = user.id
+                        const username = user.username
+                        return (
+                            <>
+                                <div key={user.id} className='individual-search-result'>
+                                    <div className='inline-container'>
+                                        <button className='login-form-buttons'>
+                                            <SearchedUserGoalModal userId={userId} username={username}/>
+                                            {user.username}
+                                            <SearchedUserGoalModal userId={userId} username={username}/>
+                                        </button>
+                                        <div>
+                                            <CheckIfFollowing follower={user}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })}
+                </div> 
+                </div>
             )
         }
+    } else {
+        return (
+            <>
+                <div className='main-container'>
+                    <SidePanel />
+                    <div className='results-container'>
+                        <h1>Please search for a user</h1>
+                    </div>
+                </div>
+            </>
+        )
     }
-
- 
-
-//     if (searchedProducts.length === 0){
-//         return (
-//             <h3>No products match the search</h3>
-//         )
-//     } else {
-//         return (
-//              <div className='product-listing'>
-//                 {searchedProducts && searchedProducts.map(product => {
-//                 return (
-//                     <>
-//                         <Link to={`/products/${product.id}`} id='link'>
-//                             <img className='product-listing' style={{width: 200, height: 200}} src ={product.ImageUrls[0].url} key={product.id} alt='' /> 
-//                             <div className='product-prices'>Title: {product.title}</div>
-//                             <div className='product-prices'> Price: ${product.price}</div>
-//                         </Link>
-//                     </>
-//                 )
-//             })}
-//             </div>
-//         )
-//     }
-// }
-
-return (
-    <h1>ho</h1>
-)
 }
 
 export default SearchPage;
