1a2549dcf6564d66fe317047e306c03f072a217f
Angelica <angelica.wilson93@gmail.com>
2021-02-12 11:30:54 -0600

    Backend route for searching

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const Sequelize = require('sequelize');
-const { QueryTypes } = require('sequelize');
 const Op = Sequelize.Op;
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
-const { Like, User, Goal } = require('../../db/models');
+const { User, Goal } = require('../../db/models');
 
-// 
+// SEARCH GOALS AND USERS
 router.get('/:searchTerm', asyncHandler(async(req, res) => {
-//   const searchTerm = req.params.searchTerm
-//   const search = await Sequelize.query("SELECT * FROM goals OR users WHERE goals.name = 'demo' OR users.username = 'demo'", { type: QueryTypes.SELECT });
+    const searchTerm = req.params.searchTerm
+    const users = await User.findAll({
+    where: {
+      [Op.or]: {
+      username: {
+        [Op.iLike]: `${searchTerm}%`
+      },
+      }
+    },
+    });
 
-//   return res.json({search})
+    const goals = await Goal.findAll({
+    where: {
+      [Op.or]: {
+      name: {
+        [Op.iLike]: `%${searchTerm}%`
+      },
+      goalType: {
+        [Op.iLike]: `%${searchTerm}%`
+      },
+      }
+    },
+    });
+
+    
+
+  return res.json({users, goals})
 }));
 
 module.exports = router;
