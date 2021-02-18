c75c17db101c7f258439dceafc8c598b56fbb2a4
Angelica <angelica.wilson93@gmail.com>
2021-02-18 16:58:21 -0600

    Moved to see if change

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,15 @@ const isProduction = environment === 'production';
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 app.use(morgan('dev'));
 
 app.use(cookieParser());
