c53bce66c74de154e45fcf4aee628dc839910e0f
Angelica <angelica.wilson93@gmail.com>
2021-02-23 20:41:51 -0600

    Work to create landing page. Set up jumbotron

---
+++ frontend/src/components/LandingPageJumbotron/index.js
@@ -0,0 +1,15 @@
+import LoginFormPage from '../LoginFormPage';
+import './LandingPageJumbotron.css';
+
+function LandingPageJumbotron  () {
+
+    return (
+        <>
+            <div className='jumbotron'>
+                <LoginFormPage />
+            </div>
+        </>
+    )
+}
+
+export default LandingPageJumbotron;
